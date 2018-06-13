c2bc98cef6921f3eed7e9376dafb1e1f00a48788
a183cute33 <a183cute33@gmail.com>
2018-06-13 10:30:49 +0800

    切版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  showAll = true;
+  searchTag = ['Koahsiung', 'Taipei', 'Learning'];
+
+  categoriesTag = [
+    { name: 'All', checked: '' },
+    { name: 'Entertainment', checked: '' },
+    { name: 'Food', checked: '' },
+    { name: 'Learning', checked: '' },
+    { name: 'Outdoors', checked: '' },
+  ];
+  data = [1, 2, 3];
 }
