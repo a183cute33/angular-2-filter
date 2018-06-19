23e360320b5cb8909bffc67723dbb6710bd9e6a2
a183cute33 <a183cute33@gmail.com>
2018-06-19 11:34:12 +0800

    修改邏輯

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import { AppService } from './service/app.service';
 import { App } from './model/app';
-
-import { ajax } from 'rxjs/ajax';
+import { combineLatest, Observable } from 'rxjs';
+import { debounceTime, mergeMap } from 'rxjs/operators';
 
 
 @Component({
@@ -10,111 +11,84 @@ import { ajax } from 'rxjs/ajax';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
   showAll = true;
-  pay: Boolean = false;
-  open: Boolean = false;
+  locationOption = [];
+  ticketInfoOptions: any[];
+  initData = [];
+  showData = [];
+  copyData = [];
+  singleData = {};
+  key = '';
+  keyword = '';
+  @ViewChild('tKeyword') tKeyword: NgModel;
 
-  searchTag = ['All'];
-  locationOption = ['All'];
-  selectedLocation = 'All';
-  oldSelectedLocation = 'All';
-  datas: App[] = [];
-  showDatas = [];
-  count = 0;
-  pageCount = 0;
+  zone = '';
+  @ViewChild('tZone') tZone: NgModel;
+
+  ticketInfo = '';
+  @ViewChild('tTicketInfo') tTicketInfo: NgModel;
 
   constructor(private appService: AppService) { }
 
   ngOnInit() {
-    const apiData = ajax('https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97');
-    apiData.subscribe(res => console.log(res.status, res.response));
-    this.appService.getData()
-      .subscribe((res: any) => {
-        const records = res.result.records;
-        this.count = records.length;
-        // this.pageCount = records.length / 10;
-        records.map((m) => {
-          if (this.locationOption.indexOf(m.Zone) < 0) {
-            this.locationOption.push(m.Zone);
-          }
-          this.datas.push(m);
+    this.appService.getData().subscribe((res: any) => {
+      const records = res.result.records;
+      this.initData = records;
+      this.ticketInfoOptions =
+        Array.from(new Set(this.initData.map(data => data.Ticketinfo))).filter(res => !!res);
+      this.locationOption = Array.from(new Set(this.initData.map(data => data.Zone))).filter(resp => !!resp);
+      this.initTheData();
+    });
+  }
+
+  initTheData() {
+    this.copyData = [...this.initData];
+    this.showData = [...this.copyData];
+  }
+
+  ngAfterViewInit() {
+    combineLatest(this.tZone.valueChanges,
+      this.tTicketInfo.valueChanges,
+      this.tKeyword.valueChanges).pipe(
+        debounceTime(500)).subscribe((value: any) => {
+          this.query(value);
         });
-        this.showDatas = this.datas.slice();
+  }
+  filterData(key, value) {
+    if (this.copyData) {
+      this.copyData = this.copyData.filter(res => {
+        return res[key].match(value);
       });
+    }
   }
-  changeSelect(event: Event) {
-    this.deleteSearchFormIndexOf(this.oldSelectedLocation);
-    this.allTagFilter();
-    this.addSearchTag(this.selectedLocation);
-    this.oldSelectedLocation = this.selectedLocation;
-  }
+  query(termArray: Array<string>) {
+    this.initTheData();
+    termArray.forEach((v, i) => {
+      console.log(v, i)
+      if (i === 0) {
+        this.key = 'Zone';
+        if (v) {
+          this.filterData(this.key, v);
+        }
+      } else if (i === 1) {
+        this.key = 'Ticketinfo';
+        if (v) {
+          this.filterData(this.key, v);
+        }
+      } else if (i === 2) {
+        this.key = 'Name';
+        if (v) {
+          this.filterData(this.key, v);
+        }
+      }
+      this.showData = [...this.copyData];
+    });
 
-  deleteSearchTag(i) {
-    this.searchTag.splice(i, 1);
-    this.allTagFilter();
   }
-
-  deleteSearchFormIndexOf(arr) {
-    const index = this.searchTag.indexOf(arr);
-    if (index > -1) {
-      this.searchTag.splice(index, 1);
-    }
-  }
-
-  addSearchTag(data) {
-    const locationDiff = this.searchTag.indexOf(data);
-    if (locationDiff === -1 && data) {
-      this.searchTag.push(data);
-    }
-  }
-
-  onSearchEnter(data) {
-    this.addSearchTag(data);
-    if (data) {
-      this.showDatas = this.datas.filter(f => f.Name === data);
-    } else {
-      this.allTagFilter();
-    }
-  }
-
-  //判斷search 是否為空，為空則顯示全部資料
-  onSpace(data) {
-    if (data === '') {
-      this.allTagFilter();
-    }
-  }
-
-  payToggle(event: Event, str: string) {
-    if (this.pay) {
-      this.deleteSearchFormIndexOf(str);
-    } else {
-      this.addSearchTag(str);
-    }
-    this.allTagFilter();
-  }
-
-  openToggle(event: Event, str: string) {
-    if (this.open) {
-      this.deleteSearchFormIndexOf(str);
-    } else {
-      this.addSearchTag(str);
-    }
-    this.allTagFilter();
-  }
-
-  allTagFilter() {
-    if (this.selectedLocation !== 'All') {
-      this.showDatas = this.datas.filter(f => f.Zone === this.selectedLocation);
-    } else {
-      this.showDatas = this.datas.slice();
-    }
-    if (this.pay) {
-      this.showDatas = this.showDatas.filter(f => f.Ticketinfo === '免費參觀');
-    }
-    if (this.open) {
-      this.showDatas = this.showDatas.filter(f => f.Opentime === '全天候開放');
-    }
+  showDetail(data) {
+    this.showAll = !this.showAll;
+    console.log(data);
+    this.singleData = data;
   }
 }
